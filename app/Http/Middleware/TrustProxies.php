82b5b8de6b224c90b178ee9c4a3d5563b89b9bb3
y.nishimura <ubuntu@ip-172-31-0-166.ap-northeast-1.compute.internal>
2021-06-06 12:16:53 +0000

    CRUD tasks

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array|string
      */
-    protected $proxies = '*'; // 全プロキシを信用
+    protected $proxies = '*';
 
     /**
      * The headers that should be used to detect proxies.
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,25 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'TasksController@index');
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::resource('tasks', 'TasksController');
+
+// Route::get('tasks/{id}', 'TasksController@show');
+
+// Route::post('tasks', 'TasksController@store');
+
+// Route::put('tasks/{id}', 'TasksController@update');
+
+// Route::delete('tasks/{id}','TasksController@destroy');
+
+
+// Route::get('tasks', 'TasksController@index')->name('tasks.index');
+
+// Route::get('tasks/create', 'TasksController@create')->('tasks.create');
+
+// Route::put('tasks/{id}', 'TasksController@edit')->('tasks.edit');
